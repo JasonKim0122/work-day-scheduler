82f7b6c21ffe8bab3c9698d098f67ce9690d8c21
JasonKim0122 <JasonKim0122@gmail.com>
2022-03-30 20:52:06 -0500

    added pseudocode and used moment() to display current date and time

---
+++ Challenges/Week FIVE/Develop/assets/js/script.js
@@ -0,0 +1,13 @@
+//display current date and time on homepage.
+$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+
+//time blocks are color coded to indicate to users whether it is in the past,present, or future
+
+
+
+//create a saveBtn that will store data into localStorage
+
+
+
+//After page is refreshed data will be pulled from localStorage and will remain on the page.
