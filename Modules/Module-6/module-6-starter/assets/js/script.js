853c3390a9afdb3b6f722f6718224f5bcc051f9a
JasonKim0122 <JasonKim0122@gmail.com>
2022-03-31 13:59:19 -0500

    added button handlers

--- Modules/Module-6/module-6-starter/assets/js/script.js
+++ Modules/Module-6/module-6-starter/assets/js/script.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.getElementById("username");
 var repoContainerEl = document.getElementById("repos-container");
 var repoSearchTermEl = document.getElementById("repo-search-term");
+var languageButtonsEl=document.getElementById("language-buttons");
 
 
 function getUserRepos (user) {
@@ -84,7 +85,30 @@ function displayRepos (repos, searchTerm) {
     }
 }
 
+function getFeaturedRepos (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: GitHub User not found");
+        }
+    });
+};
+
+function buttonClickHandler (event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if(language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent="";
+    }
+};
+
 getUserRepos();
 
 //event listeners 
 userFormEl.addEventListener('click', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
