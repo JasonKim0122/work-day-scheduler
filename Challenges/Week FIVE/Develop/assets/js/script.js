0072882445f6b79ab606fcc4f10365713e53045c
JasonKim0122 <JasonKim0122@gmail.com>
2022-03-31 14:24:15 -0500

    added color code to time blocks

--- Challenges/Week FIVE/Develop/assets/js/script.js
+++ Challenges/Week FIVE/Develop/assets/js/script.js
@@ -1,9 +1,26 @@
+//Variables
+var timeBlock = document.querySelector(".time-block");
+
 //display current date and time on homepage.
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 
 //time blocks are color coded to indicate to users whether it is in the past,present, or future
+function timeBlockColor () {
+    var timeHour = moment().hours();
 
+    timeBlock.each(function() {
+        var currentTime = parseInt($(this).attr("id"));
+
+        if(currentTime > timeHour) {
+            timeBlock.classList.add("future");
+        } else if ( currentTime === timeHour) {
+            timeBlock.classList.add("present");
+        } else {
+            timeBlock.classList.add("past");
+        }
+    });
+};
 
 
 //create a saveBtn that will store data into localStorage
